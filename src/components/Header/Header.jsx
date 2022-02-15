15c069ac91c437bbdb52f502804e175bd0625f48
Clay Webb <clay_webb@outlook.com>
2022-02-14 19:42:01 -0600

    This will display the header of the page on the DOM

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,9 @@
+// This will append the header of the page
+function Header () {
+    return (
+            <h1>To Do List</h1>
+    )
+}
+
+// Export the function to App.jsx
+export default Header;
